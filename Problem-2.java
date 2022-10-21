d42820951a9d654e95986f33c16e16c67bb400f2
mithunrm <116357501+mithunrm@users.noreply.github.com>
2022-10-21 22:21:31 +0530

    Create Problem-2.java
    I am Mithun.I written this Program in java language

---
+++ Problem-2.java
@@ -0,0 +1,21 @@
+package selenium;
+
+import java.util.Scanner;
+
+public class p1 {
+public static void main(String[] args) {
+	System.out.println("Enter the a value");
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+
+	for (int i = 1; i <=a*2; i++)
+	{	
+		if((i%2!=0))
+		{
+			System.out.println(i);
+		}
+	
+		
+	}
+}
+}
